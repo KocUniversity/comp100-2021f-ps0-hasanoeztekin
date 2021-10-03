c98205c66a528c02c21b441dbcb8c91b3002c52d
HASAN OZTEKIN <91706700+hasanoeztekin@users.noreply.github.com>
2021-10-03 19:12:48 +0000

    My first steps.

---
+++ main.py
@@ -0,0 +1,6 @@
+v1 = int(input("enter a number for x:"))
+v2 = int(input("enter a number for y:"))
+print("x**y=",int(pow(v1,v2)))
+import math
+print("math.log2(x)=",math.log(v1,2))
+print("0076577")
